0a7962e433cc8023beee88451e58b86071e5b816
旗舰小白 <ad2156@email.vccs.edu>
2019-01-29 09:18:27 +0800

    2019/1/29/9:29

--- xfz/apps/cms/urls.py
+++
@@ -1,9 +0,0 @@
-#encoding: utf-8
-from django.urls import path
-from . import views
-
-app_name = 'cms'
-
-urlpatterns = [
-    path('login/', views.login_view, name='login')
-]
--- xfz/apps/news/urls.py
+++
@@ -1,9 +0,0 @@
-#encoding: utf-8
-from django.urls import path
-from . import views
-
-app_name = 'news'
-
-urlpatterns = [
-    path('', views.index,name='index')
-]
