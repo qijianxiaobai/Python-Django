3164e08b7546aba1758f1715a5e54d8fa06abc49
旗舰小白 <ad2156@email.vccs.edu>
2019-01-29 11:04:49 +0800

    2019/1/29/11:04

--- ONE/ONE/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
-
-def index(request):
-    # return HttpResponse('Hellow World!')
-    return render(request, 'index.html')
