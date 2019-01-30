1088ac9f99e3630fcc5b153e6efad973ee4fd03b
旗舰小白 <ad2156@email.vccs.edu>
2019-01-30 16:56:51 +0800

    2019/1/30/16:55

--- xfz/xfz/urls.py
+++ xfz/xfz/urls.py
@@ -1,7 +1,7 @@
 """xfz URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,11 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
-
+from django.urls import path,include
+from apps.news import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('cms/', include("apps.cms.urls")),
-    path('', include("apps.news.urls")),
+    path('',views.index,name='index'),
+    path('search/',views.search,name='search'),
+    path('news/', include("apps.news.urls")),
+    path('cms/',include('apps.cms.urls')),
     path('account/', include("apps.xfzauth.urls")),
-]
+    path('course/',include('apps.course.urls')),
+    path('payinfo/',include('apps.payinfo.urls')),
+    path('ueditor/',include('apps.ueditor.urls'))
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns.append(path("__debug__/",include(debug_toolbar.urls)))
